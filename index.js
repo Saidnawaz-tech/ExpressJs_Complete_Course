fb995a96d61cdc917d017f207d6f99941d7409ad
Said Nawaz <saidbro4442@gmail.com>
2025-08-12 06:28:01 +0500

    Pratice, Routing, middleware Topics

--- index.js
+++ index.js
@@ -1,44 +1,56 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const userRoutes = require('./routes/userRoutes');
 
-app.use(express.json()); // Middleware to parse JSON
-app.use('/api/users', userRoutes); // Route prefix
+// Advance Routing (different HTTP methods for same Route)
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.use(express.json())
 
+app.get('/students', (req, res)=>{
+  res.json({message: 'Get Request for students'})
+})
+app.post('/students', (req, res)=>{
+  res.json({message: 'Post request for students', data: req.body})
+})
+app.put('/students/:id', (req, res)=>{
+  res.json({message: `Student with id ${req.params.id} is updated`})
+})
+app.delete('/students/:id', (req, res)=>{
+  res.json({message: `Student with id ${req.params.id} is deleted`})
+})
+// Route parameters
+app.get('/students/:id', (req, res)=>{
+  res.send(`Get Student with ID ${req.params.id}`)
+})
 
+// Query Strings
+app.get('/search', (req, res)=>{
+  const {name, age} = req.query
+  res.json({name, age}) 
+})
 
+// Custom middleware Flow
+function logTime(req, res, next){
+  const logtime = new Date().toLocaleTimeString();
+  console.log('Time: ', logtime); // time displayed in terminal
+  next();
+}
+app.use(logTime)
+app.get('/', (req, res)=>{
+  res.send('Hello world! Home Page')
+})
 
+// chaining middleware functions
+function first(req, res, next){
+  console.log('First Middleware')
+  next();
+}
+function second(req, res, next){
+  console.log('First Middleware')
+  next();
+}
+app.get('/test', first, second, (req, res)=>{
+  console.log('final response')
+})
 
+app.listen(3000, ()=>{ console.log('Server is running at http://localhost:3000/students')})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const myexp = express();
-// const port = 3000;
-
-// myexp.get('/', (req, res)=>{
-//     res.send('Home page')
-// })
-// myexp.get('/about', (req, res)=>{
-//     res.sendFile(path.join(__dirname, 'index.html'))
-//     // res.statusCode(500)
-// })
-// myexp.listen(port, ()=>{
-//     console.log(`server running at http://localhost:${port}`)
-// })
